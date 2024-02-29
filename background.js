633fc0301ea3b11290b190a9f4bba72bca4f5474
Prateek Goyal <gprateek015@gmail.com>
2024-02-29 16:44:31 +0530

    upgraded to v3

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
+const AUTH_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.NjVjYjc3NThlMDQyYjhkZDdmZWJkNGVk.zrDkOe04Cqb4j_5654ZzXrGmAaTHExhTx9apItl2yh0';
+const API_URL = 'https://api.resumer.cloud';
+
 const fetchResumeData = async ({ job_description }) => {
-  const getResumeDataApiUrl =
-    'http://localhost:8000/resume/data-new?rewrite=false';
+  const getResumeDataApiUrl = `${API_URL}/resume/data-new?rewrite=${!!job_description?.length}`;
   const resp = await fetch(getResumeDataApiUrl, {
     method: 'POST',
     headers: {
-      authorization:
-        'eyJhbGciOiJIUzI1NiJ9.NjUwYmZkMGU5MDE1ZmI4MzQ4OTFkZDdm.qgRxfHTLdhJCamd950gQJA4-jHUjs4XlUmEQ8bg4k-Q',
+      authorization: AUTH_TOKEN,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ job_description })
@@ -15,13 +17,12 @@ const fetchResumeData = async ({ job_description }) => {
 };
 
 const fetchPdfArray = async ({ resumeData }) => {
-  const apiUrl = 'http://localhost:8000/resume/engineering/0/load';
+  const apiUrl = `${API_URL}/resume/engineering/0/load`;
   const response = await fetch(apiUrl, {
     method: 'POST',
     body: JSON.stringify(resumeData),
     headers: {
-      authorization:
-        'eyJhbGciOiJIUzI1NiJ9.NjUwYmZkMGU5MDE1ZmI4MzQ4OTFkZDdm.qgRxfHTLdhJCamd950gQJA4-jHUjs4XlUmEQ8bg4k-Q',
+      authorization: AUTH_TOKEN,
       'Content-Type': 'application/json'
     }
   });
@@ -31,43 +32,93 @@ const fetchPdfArray = async ({ resumeData }) => {
   return response.arrayBuffer();
 };
 
+let value = '';
+
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   try {
-    if (message.action === 'downloadPDF') {
+    if (message.method === 'downloadPDF') {
       const { job_description } = message;
 
       const resumeData = await fetchResumeData({ job_description });
 
       const pdfArrayBuffer = await fetchPdfArray({ resumeData });
 
-      const pdfBlob = new Blob([pdfArrayBuffer], {
+      const blob = new Blob([pdfArrayBuffer], {
         type: 'application/pdf'
       });
-      const url = URL.createObjectURL(pdfBlob);
 
-      chrome.downloads.download({
-        url: url,
-        filename: 'Resume.pdf'
-      });
-    } else if (message.action === 'getJobDescription') {
-      console.log('message', message);
-
-      // Request job description from content script
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        chrome.tabs.sendMessage(
-          activeTab.id,
-          { action: 'extractJobDescription' },
-          function (response) {
-            console.log('response', response);
-            // Send the job description to the popup
-            chrome.runtime.sendMessage({
-              action: 'jobDescriptionUpdated',
-              job_description: response.jobDescription
-            });
+      // use BlobReader object to read Blob data
+      const reader = new FileReader();
+      reader.onload = () => {
+        const buffer = reader.result;
+        const blobUrl = `data:${blob.type};base64,${btoa(
+          new Uint8Array(buffer).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ''
+          )
+        )}`;
+        chrome.downloads.download(
+          {
+            url: blobUrl,
+            filename: 'Resume.pdf',
+            // saveAs: true,
+            conflictAction: 'uniquify'
+          },
+          () => {
+            sendResponse({ success: true });
           }
         );
-      });
+      };
+      reader.readAsArrayBuffer(blob);
+    } else {
+      console.log(message);
+      switch (message.method) {
+        case 'set':
+          value = trimJobDescription(message.value[0]);
+          console.log(value);
+          sendResponse({ value: null });
+          break;
+        case 'get':
+          sendAfterSet();
+          break;
+        case 'clear':
+          value = '';
+          sendResponse({ value: null });
+          break;
+      }
+      return true;
+
+      async function sendAfterSet() {
+        for (let i = 0; i < 10; i++) {
+          if (value != '') {
+            sendResponse({ value: value });
+            return;
+          }
+          console.log('Start Sleep 1s.');
+          await new Promise(s => setTimeout(s, 1000));
+          console.log('End Sleep 1s.');
+        }
+        value = 'Error: Document information could not be obtained.';
+      }
+
+      function trimJobDescription(value) {
+        // return value.slice(value.find('About the job'));
+        let resp = value.slice(value.indexOf('About the job'));
+        const regexPattern =
+          /Qualifications\s+(\d+)\s+of\s+(\d+)\s+skills\s+match\s+your\s+profile/;
+        const match = resp.match(regexPattern);
+        if (match) {
+          // Get the starting position of the match
+          const matchIndex = match.index;
+
+          // Slice the string until the starting position of the match
+          const slicedString = resp.slice(0, matchIndex);
+
+          return slicedString;
+        } else {
+          return slicedString;
+        }
+      }
     }
   } catch (err) {
     console.error(err);
--- popup.js
+++ popup.js
@@ -1,25 +1,70 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Request the job description from the background script
-  chrome.runtime.sendMessage({ action: 'getJobDescription' });
-});
-
-// Handle the message when the job description is updated
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === 'jobDescriptionUpdated') {
+document
+  .getElementById('download-button')
+  .addEventListener('click', async () => {
     const jobDescriptionInput = document.getElementById('job-description');
-    jobDescriptionInput.value = message.job_description;
-  }
-});
 
-document.getElementById('download-button').addEventListener('click', () => {
-  const jobDescriptionInput = document.getElementById('job-description');
+    document.getElementById('download-button').innerText = 'Processing...';
+    const resp = await chrome.runtime.sendMessage({
+      method: 'downloadPDF',
+      job_description: jobDescriptionInput.value
+    });
+    if (resp?.success) {
+      document.getElementById('download-button').innerText = 'Download';
+    }
+  });
 
-  chrome.runtime.sendMessage({
-    action: 'downloadPDF',
-    job_description: jobDescriptionInput.value
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.runtime.sendMessage({ method: 'clear' }, () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          function: getDocumentInfo
+        },
+        result => {
+          if (chrome.runtime.lastError) {
+            document.getElementById('job-description').value =
+              'Error: ' + chrome.runtime.lastError.message;
+          } else {
+            chrome.runtime.sendMessage({ method: 'get' }, response => {
+              document.getElementById('job-description').value = response.value;
+            });
+          }
+        }
+      );
+    });
   });
 });
 
-document.getElementById('signup-button').addEventListener('click', () => {
-  window.location = 'localhost:3000';
-});
+function getDocumentInfo() {
+  let elements;
+  let host = window.location.host;
+
+  // Check the host to determine the appropriate class name
+  if (host.includes('linkedin')) {
+    elements = document.getElementsByClassName(
+      'jobs-search__job-details--wrapper'
+    );
+  } else if (host.includes('glassdoor')) {
+    elements = document.getElementsByClassName(
+      'JobDetails_jobDescriptionWrapper__BTDTA'
+    );
+  } else {
+    // Handle other hosts or provide a default class name
+    elements = document.getElementsByClassName('default-class-name');
+  }
+
+  let plainTextContents = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    let plainTextContent = elements[i].innerText || elements[i].textContent;
+    plainTextContents.push(plainTextContent.trim());
+  }
+
+  chrome.runtime.sendMessage(
+    { method: 'set', value: plainTextContents },
+    msg => {
+      console.log(msg);
+    }
+  );
+}
